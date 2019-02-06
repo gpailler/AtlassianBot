b85f98dbf5dc610861df43723f898591bbb391f4
Fred Leboucher <kettch@users.noreply.github.com>
2019-02-06 05:05:44 +0100

    Added a fallback to identify users whose user name doesn't match in Slack & Crucible (#18)
    Added a fallback to identify users whose user name doesn't match in Slack & Crucible

--- utils/slackbot_utils.py
+++ utils/slackbot_utils.py
@@ -2,7 +2,6 @@ import inspect
 
 from slackbot.bot import Bot
 
-
 def get_slackclient():
     stack = inspect.stack()
     for frame in [f[0] for f in stack]:
@@ -10,3 +9,11 @@ def get_slackclient():
             instance = frame.f_locals['self']
             if isinstance(instance, Bot):
                 return instance._client
+
+def get_user_by_crucible_handle(slack_client, username, field_code):
+    for userid, user in slack_client.users.items():
+        try:
+            if user['profile']['fields'][field_code]['value'] == username:
+                return userid
+        except:
+            pass
